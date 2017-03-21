0dbd0bd9f756ae23640f35464afb92194628a2a0
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-03-21 16:14:04 +0100

    Handle types and signatures in isFunctionScopeBoundary

--- src/utils.ts
+++ src/utils.ts
@@ -194,17 +194,25 @@ export function isScopeBoundary(node: ts.Node): ScopeBoundary {
 
 export function isFunctionScopeBoundary(node: ts.Node): boolean {
     switch (node.kind) {
-        case ts.SyntaxKind.FunctionDeclaration:
         case ts.SyntaxKind.FunctionExpression:
         case ts.SyntaxKind.ArrowFunction:
-        case ts.SyntaxKind.MethodDeclaration:
         case ts.SyntaxKind.Constructor:
-        case ts.SyntaxKind.GetAccessor:
-        case ts.SyntaxKind.SetAccessor:
         case ts.SyntaxKind.ModuleDeclaration:
         case ts.SyntaxKind.ClassDeclaration:
         case ts.SyntaxKind.ClassExpression:
         case ts.SyntaxKind.EnumDeclaration:
+        case ts.SyntaxKind.MethodDeclaration:
+        case ts.SyntaxKind.FunctionDeclaration:
+        case ts.SyntaxKind.GetAccessor:
+        case ts.SyntaxKind.SetAccessor:
+        case ts.SyntaxKind.InterfaceDeclaration:
+        case ts.SyntaxKind.TypeAliasDeclaration:
+        case ts.SyntaxKind.MethodSignature:
+        case ts.SyntaxKind.CallSignature:
+        case ts.SyntaxKind.ConstructSignature:
+        case ts.SyntaxKind.ConstructorType:
+        case ts.SyntaxKind.FunctionType:
+        case ts.SyntaxKind.MappedType:
             return true;
         case ts.SyntaxKind.SourceFile:
             // if SourceFile is no module, it contributes to the global scope and is therefore no scope boundary
