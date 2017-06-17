398d583ade12fed18e46637ba7c25b1305f3ffd5
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-06-17 10:23:50 +0200

    isFunctionWithBody: handle constructor overload signature

--- util/util.ts
+++ util/util.ts
@@ -339,9 +339,9 @@ export function isFunctionWithBody(node: ts.Node): node is ts.FunctionLikeDeclar
         case ts.SyntaxKind.SetAccessor:
         case ts.SyntaxKind.FunctionDeclaration:
         case ts.SyntaxKind.MethodDeclaration:
+        case ts.SyntaxKind.Constructor:
             return (<ts.FunctionLikeDeclaration>node).body !== undefined;
         case ts.SyntaxKind.FunctionExpression:
-        case ts.SyntaxKind.Constructor:
         case ts.SyntaxKind.ArrowFunction:
             return true;
         default:
