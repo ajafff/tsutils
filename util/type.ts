87f2d10bfe9038a530f30dcdc5b76498b0e01e75
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-11-04 17:43:15 +0100

    Implement isEmptyObjectType

---
+++ util/type.ts
@@ -0,0 +1,14 @@
+import * as ts from 'typescript';
+
+export function isEmptyObjectType(type: ts.Type): type is ts.ObjectType {
+    if (type.flags & ts.TypeFlags.Object &&
+        type.getProperties().length === 0 &&
+        type.getCallSignatures().length === 0 &&
+        type.getConstructSignatures().length === 0 &&
+        type.getStringIndexType() === undefined &&
+        type.getNumberIndexType() === undefined) {
+        const baseTypes = type.getBaseTypes();
+        return baseTypes === undefined || baseTypes.every(isEmptyObjectType);
+    }
+    return false;
+}
