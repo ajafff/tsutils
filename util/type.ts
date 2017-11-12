1fd1215be0cd4565f99d4aadae269a5a500c9418
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2017-11-12 23:11:39 +0100

    isEmptyObjectType: only consider anonymous types as empty

--- util/type.ts
+++ util/type.ts
@@ -2,6 +2,7 @@ import * as ts from 'typescript';
 
 export function isEmptyObjectType(type: ts.Type): type is ts.ObjectType {
     if (type.flags & ts.TypeFlags.Object &&
+        (<ts.ObjectType>type).objectFlags & ts.ObjectFlags.Anonymous &&
         type.getProperties().length === 0 &&
         type.getCallSignatures().length === 0 &&
         type.getConstructSignatures().length === 0 &&
