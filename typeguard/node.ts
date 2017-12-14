560f3ef1a2196a836dc82273c05555e2b03e02db
Doug Miller <doug@dougmiller.io>
2017-12-14 10:37:19 -0500

    Fix typo for isJsxFragment. (#17)

--- typeguard/node.ts
+++ typeguard/node.ts
@@ -357,7 +357,12 @@ export function isJsxExpression(node: ts.Node): node is ts.JsxExpression {
     return node.kind === ts.SyntaxKind.JsxExpression;
 }
 
+/** @deprecated use `isJsxFragment` instead. */
 export function isJsxFramgment(node: ts.Node): node is ts.JsxFragment {
+    return isJsxFragment(node);
+}
+
+export function isJsxFragment(node: ts.Node): node is ts.JsxFragment {
     return node.kind === ts.SyntaxKind.JsxFragment;
 }
 
