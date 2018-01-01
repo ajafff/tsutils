35146ecfe255712971035aa965e7e127fdce4e98
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-01-01 18:08:41 +0100

    Add documentation

--- util/convert-ast.ts
+++ util/convert-ast.ts
@@ -1,38 +1,28 @@
 import * as ts from 'typescript';
 
+/** Wraps an AST node. Can be used as a tree using `children` or a linked list using `next` and `skip`. */
 export interface NodeWrap {
+    /** The real AST node. */
     node: ts.Node;
+    /** All immediate children of `node` that would be visited by `ts.forEachChild(node, cb)`. */
     children: NodeWrap[];
+    /** Link to the next NodeWrap, depth-first. */
     next?: NodeWrap;
+    /** Link to the next NodeWrap skipping all children of the current node. */
     skip?: NodeWrap;
+    /** Link to the parent NodeWrap */
     parent?: NodeWrap;
 }
 
-export function flattenAst(sourceFile: ts.SourceFile): ts.Node[] {
-    return getConvertedAst(sourceFile).flat;
-}
-
-export function wrapAst(sourceFile: ts.SourceFile): NodeWrap {
-    return getConvertedAst(sourceFile).wrapped;
-}
-
-const CACHE = new WeakMap<ts.SourceFile, ConvertedAst>();
-
-interface ConvertedAst {
+export interface ConvertedAst {
+    /** nodes wrapped in a data structure with useful links */
     wrapped: NodeWrap;
-    flat: ts.Node[];
+    /** depth-first array of all nodes */
+    flat: ReadonlyArray<ts.Node>;
 }
 
-function getConvertedAst(sourceFile: ts.SourceFile): ConvertedAst {
-    let result = CACHE.get(sourceFile);
-    if (result !== undefined)
-        return result;
-    result = convertAst(sourceFile);
-    CACHE.set(sourceFile, result);
-    return result;
-}
-
-function convertAst(sourceFile: ts.SourceFile): ConvertedAst {
+/** Takes a `ts.SourceFile` and creates data structures that are easier (or more performant) to traverse. */
+export function convertAst(sourceFile: ts.SourceFile): ConvertedAst {
     const wrapped: NodeWrap = {
         node: sourceFile,
         parent: undefined,
@@ -43,7 +33,7 @@ function convertAst(sourceFile: ts.SourceFile): ConvertedAst {
     const flat: ts.Node[] = [];
     let current = wrapped;
     let previous = current;
-    ts.forEachChild(sourceFile, function wrap(node: ts.Node) {
+    ts.forEachChild(sourceFile, function wrap(node) {
         flat.push(node);
         const parent = current;
         previous.next = current = {
