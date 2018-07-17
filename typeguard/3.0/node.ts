3c9eb4aecf8a35eead32b862cf8ee40e99460c8b
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-07-17 22:28:19 +0200

    add typeguards for 3.0.0

--- typeguard/3.0/node.ts
+++ typeguard/3.0/node.ts
@@ -1,1 +1,15 @@
 export * from '../2.9/node';
+
+import * as ts from 'typescript';
+
+export function isOptionalTypeNode(node: ts.Node): node is ts.OptionalTypeNode {
+    return node.kind === ts.SyntaxKind.OptionalType;
+}
+
+export function isRestTypeNode(node: ts.Node): node is ts.RestTypeNode {
+    return node.kind === ts.SyntaxKind.RestType;
+}
+
+export function isSyntheticExpression(node: ts.Node): node is ts.SyntheticExpression {
+    return node.kind === ts.SyntaxKind.SyntheticExpression;
+}
