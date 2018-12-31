2e9f9a5883aff48c4a928b1e4896bbcc38a76b12
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2018-12-31 15:10:42 +0100

    add getCheckJsDirective

--- util/util.ts
+++ util/util.ts
@@ -1306,3 +1306,18 @@ export function isCompilerOptionEnabled(options: ts.CompilerOptions, option: Boo
 export function isAmbientModule(node: ts.ModuleDeclaration): boolean {
     return node.name.kind === ts.SyntaxKind.StringLiteral || (node.flags & ts.NodeFlags.GlobalAugmentation) !== 0;
 }
+
+/** @returns the last `// @ts-check` or `// @ts-nocheck` directive in the given file. */
+export function getCheckJsDirective(source: string) {
+    let directive: ts.CheckJsDirective | undefined;
+    // needs to work around a shebang issue until https://github.com/Microsoft/TypeScript/issues/28477 is resolved
+    ts.forEachLeadingCommentRange(source, (ts.getShebang(source) || '').length, (pos, end, kind) => {
+        if (kind === ts.SyntaxKind.SingleLineCommentTrivia) {
+            const text = source.slice(pos, end);
+            const match = /^\/{2,3}\s*@ts-(no)?check(?:\s|$)/i.exec(text);
+            if (match !== null)
+                directive = {pos, end, enabled: match[1] === undefined};
+        }
+    });
+    return directive;
+}
