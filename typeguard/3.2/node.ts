18c76effbbf6a0ac5d9b7830f87a59b22e58516d
Noam <noamyogev84@gmail.com>
2019-03-10 20:04:36 +0200

    add isNullLiteral and isBooleanLiteral

--- typeguard/3.2/node.ts
+++ typeguard/3.2/node.ts
@@ -5,3 +5,11 @@ import * as ts from 'typescript';
 export function isBigIntLiteral(node: ts.Node): node is ts.BigIntLiteral {
     return node.kind === ts.SyntaxKind.BigIntLiteral;
 }
+
+export function isNullLiteral(node: ts.Node): node is ts.NullLiteral {
+    return node.kind === ts.SyntaxKind.NullKeyword;
+}
+
+export function isBooleanLiteral(node: ts.Node): node is ts.BooleanLiteral {
+    return node.kind === ts.SyntaxKind.TrueKeyword || node.kind === ts.SyntaxKind.FalseKeyword;
+}
