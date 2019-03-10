71815d013a2cdf69daa1ebd41390653f34abfc52
Noam <noamyogev84@gmail.com>
2019-03-10 21:42:16 +0200

    move isNullLiteral, isBooleanLiteral to typeguard\2.8

--- typeguard/3.2/node.ts
+++ typeguard/3.2/node.ts
@@ -5,11 +5,3 @@ import * as ts from 'typescript';
 export function isBigIntLiteral(node: ts.Node): node is ts.BigIntLiteral {
     return node.kind === ts.SyntaxKind.BigIntLiteral;
 }
-
-export function isNullLiteral(node: ts.Node): node is ts.NullLiteral {
-    return node.kind === ts.SyntaxKind.NullKeyword;
-}
-
-export function isBooleanLiteral(node: ts.Node): node is ts.BooleanLiteral {
-    return node.kind === ts.SyntaxKind.TrueKeyword || node.kind === ts.SyntaxKind.FalseKeyword;
-}
