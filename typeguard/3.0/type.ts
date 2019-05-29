0167ec8f938f0bda4ab3d7d59fde49baee1fdc0a
Klaus Meinhardt <klaus.meinhardt1@gmail.com>
2019-05-29 18:37:00 +0200

    fix circular import

--- typeguard/3.0/type.ts
+++ typeguard/3.0/type.ts
@@ -1,7 +1,7 @@
 export * from '../2.9/type';
 
 import * as ts from 'typescript';
-import { isTypeReference } from '../3.2';
+import { isTypeReference } from '../2.9/type';
 
 export function isTupleType(type: ts.Type): type is ts.TupleType {
     return (type.flags & ts.TypeFlags.Object && (<ts.ObjectType>type).objectFlags & ts.ObjectFlags.Tuple) !== 0;
